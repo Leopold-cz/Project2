eba82aff78a98a13efaf77f6535354318946614a
15095 <15095@LAPTOP-DV35LAK5>
2019-09-05 17:12:24 +0800

    模板

--- Project2/源.cpp
+++ Project2/源.cpp
@@ -1,16 +1,38 @@
 #include <stdio.h>
-#include <AclAPI.h>
+#include <stdlib.h>
+#include <time.h>
+#include <windows.h>
+#include<string.h>
+#include <iostream>
+#include<conio.h>
+#define random(x) (rand()%x)
 
+using namespace std;
+void display(int x);
 
-int main()
-{
-	printf("hello word");
-	printf("hello word");
-	printf("hello word");
-	printf("hello word");
-	printf("hello word");
-	printf("hello word");
-	printf("hello word");
-	printf("hello word");
+int main() {
 
+	srand((int)time(0));
+	int Num = 0;
+	cout << endl << endl << "              按键开始" << endl << "        Press any key to Start";
+	_getch();
+	system("cls");
+
+	char c;
+	while (1) {
+		Num = random(400);
+		display(Num);
+		Sleep(50);
+		if (GetAsyncKeyState('P')) {
+			system("pause");
+		}
+		system("cls");
+	}
+	return 0;
+}
+
+void display(int x) {
+	cout << endl << endl << "              按p键暂停" << endl << "        Press ‘p’ key to Suspend";
+	cout << endl << endl;
+	cout << "           luck Number:" << x << endl << endl << endl;
 }
